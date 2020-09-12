1a4cfe566bba06bc36a28ee7d8fd160694d44871
Przemysław Kułaga <przemyslaw.kulaga@icloud.com>
2020-09-12 21:55:03 +0200

    - added activityIndicator while loading website

--- RepoWatcher/Controllers/WebViewController.swift
+++ RepoWatcher/Controllers/WebViewController.swift
@@ -10,23 +10,43 @@ import UIKit
 import WebKit
 
 class WebViewController: UIViewController {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     var webView: WKWebView!
     
     override func loadView() {
-        webView = WKWebView()
+        super.loadView()
+        webView = WKWebView(frame: CGRect(x: view.frame.minX, y: view.frame.minY, width: view.frame.width, height: view.frame.height))
         webView.navigationDelegate = self
-        view = webView
+        view.addSubview(webView)
+    }
+    
+    func showActivityIndicator(show: Bool) {
+        show ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+        webView.isHidden = show
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.barTintColor = UIColor.green
-
+        
         let url = URL(string: "https://www.cdaction.pl")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 }
 
-extension WebViewController: WKNavigationDelegate { }
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        showActivityIndicator(show: false)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showActivityIndicator(show: true)
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        showActivityIndicator(show: false)
+    }
+}
